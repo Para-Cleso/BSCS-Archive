4e23049669148c64cb211a0ff968c80d9afec320
Para-Cleso <mataisryushi@gmail.com>
2026-02-03 11:35:49 +0800

    Update EnrollmentSystem.java
    Added: refreshTable() and imports
    
    refreshTable to show enrollee details right from the GUI. Reads the outputted file named "EnrolledStudents.txt"

--- EnrollmentSystem.java
+++ EnrollmentSystem.java
@@ -2,38 +2,47 @@ package dreamlast;
 
 import java.io.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class Fr extends JFrame {
 
  //important variables
+
   static String stdnName, address, course, lastSchool;
   static int age;
   static JTextField txtStdnName, txtAddress, txtCourse, txtAge, txtLastSchool;
 
+  static DefaultTableModel model;
+  static JTable table;
+  static JScrollPane scrollPane;
+
  public static void main(String[] args) {
     new Fr();
- }
 
-  //gui for enrollment system
-  Fr(){
+    }
+
+  
+    //gui for enrollment system
+
+Fr(){
+
 
   //header texts
 
   JLabel lblStdnName = new JLabel("Student name");
-  add(lblStdnName).setBounds (50, 30, 188, 27); //text for name
+  add(lblStdnName).setBounds   (50, 30, 188, 27);      //text for name
 
   JLabel lblAddress = new JLabel("Address");
-  add(lblAddress).setBounds  (50, 90, 188, 27); //text for address
+  add(lblAddress).setBounds    (50, 90, 188, 27);      //text for address
 
   JLabel lblCourse = new JLabel("Course");
-  add(lblCourse).setBounds  (50, 150, 188, 27); //text for course
+  add(lblCourse).setBounds     (50, 150, 188, 27);     //text for course
 
   JLabel lblAge = new JLabel("Age");
-  add(lblAge).setBounds    (50, 210, 188, 27); //text for age
+  add(lblAge).setBounds        (50, 210, 188, 27);     //text for age
 
   JLabel lblLastSchool = new JLabel("Last School Attended");
-  add(lblLastSchool).setBounds (50, 270, 188, 27); //text for last school attended
-
+  add(lblLastSchool).setBounds (50, 270, 188, 27);     //text for last school attended
 
 
 
@@ -41,19 +50,19 @@ public class Fr extends JFrame {
 
 
   txtStdnName = new JTextField("Name");
-  add(txtStdnName).setBounds (50, 60, 188, 27); //textfield for name
+  add(txtStdnName).setBounds   (50, 60, 188, 27);       //textfield for name
 
   txtAddress = new JTextField("Address");
-  add(txtAddress).setBounds  (50, 120, 188, 27); //textfield for address
+  add(txtAddress).setBounds    (50, 120, 188, 27);      //textfield for address
 
   txtCourse = new JTextField("Course");
-  add(txtCourse).setBounds  (50, 180, 188, 27); //textfield for course
+  add(txtCourse).setBounds     (50, 180, 188, 27);      //textfield for course
 
   txtAge = new JTextField("Age");
-  add(txtAge).setBounds    (50, 240, 188, 27); //textfield for age
+  add(txtAge).setBounds        (50, 240, 188, 27);      //textfield for age
 
   txtLastSchool = new JTextField("Last School");
-  add(txtLastSchool).setBounds (50, 300, 188, 27); //textfield for last school attended
+  add(txtLastSchool).setBounds (50, 300, 188, 27);      //textfield for last school attended
 
 
   //buttons
@@ -64,6 +73,21 @@ public class Fr extends JFrame {
   add(btnEnroll).setBounds(70,360,126,34); //button for enrolling
 
 
+
+   //gui for showing enrollees
+
+    String[] columns = {"Name", "Address", "Course", "Age", "Last School"};
+    
+    model =         new DefaultTableModel(columns, 0);
+    table =         new JTable(model);
+    scrollPane =    new JScrollPane(table);
+
+    add(scrollPane).setBounds(250, 50, 400, 300);
+
+
+
+
+   //adds a function to button
 
   btnEnroll.addActionListener(e->{
 
@@ -85,31 +109,32 @@ public class Fr extends JFrame {
 
     } catch (Exception xe) {
 
-      JOptionPane.showMessageDialog(null, "Lmao");
-
+      JOptionPane.showMessageDialog(null, "Lmao " + xe);
     }
 
-  });
+    refreshTable();
 
-
+    });
 
 
   //gui enabler idk
 
-
-
+  
   setLayout(null);
+
   setTitle("JU Enrollment System");
+
   setSize(750,600);
+
   setVisible(true);
+
   setResizable(true);
+
   setLocationRelativeTo(null);
+
   setDefaultCloseOperation(EXIT_ON_CLOSE);
 
- }
-
-
-
+    }
 
 
  //writes a file for all enrollees
@@ -119,18 +144,15 @@ public class Fr extends JFrame {
   FileWriter file = new FileWriter("EnrolledStudents.txt",true);
   BufferedWriter bw = new BufferedWriter(file);
 
-  bw.write(stdnName + " | " + address + " | " + course + " | " + age + " | " + lastSchool);
+  bw.write(stdnName + "#" + address + "#" + course + "#" + age + "#" + lastSchool);
   bw.write("\n");
   bw.close();
 
   JOptionPane.showMessageDialog(null, "Enrolled!");
 
- }
+    }
 
-
-
-
-
+    
  //after enrolling, empties the field
 
 public static void setTextField(){
@@ -141,19 +163,31 @@ public static void setTextField(){
 
  txtAge.setText("");
 
- txtCourse.setText("");
+ txtCourse.setText ("");
 
  txtLastSchool.setText("");
 
+    }
 
 
-}
+//displays the information of the enrollees
 
+public static void refreshTable(){
+    model.setRowCount(0);
 
+    try(
+        BufferedReader br = new BufferedReader(new FileReader("EnrolledStudents.txt"))
+        ){
+            String line;
+            while((line = br.readLine()) != null) {
+                String[] row = line.split("#");
+                model.addRow(row);
+            }
+        }
 
-
-
-
-
+    catch(Exception r){
+        JOptionPane.showMessageDialog(null, "Lmao " + r);
+        }
+    }
 }
 
